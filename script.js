64bb534526acac4061cdeb9fd9e094ba6d0e8acd
pratiksgawande <153907666+pratiksgawande@users.noreply.github.com>
2024-09-28 08:58:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let fname ='pratik';
+let tilak = prompt('Enter your name :');
+
+ 
+if(tilak == fname){
+    alert('it is correct one');
+}
+else{
+    alert('not  correct');
+}
+let arr = [23,24,25,26,27,28,29,30];
+console.log(arr);
+
+console.log(arr.slice(0,-2));
+
+
